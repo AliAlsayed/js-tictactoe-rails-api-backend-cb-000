385e317007923618620cf0f3703630ddbfd93457
Ali Alsayed <a_alsayed87@hotmail.com>
2019-09-18 10:44:52 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,7 @@ class GamesController < ApplicationController
   end
 
   def update
-    @game = Game.update(params)
+    @game = Game.update(game_params)
     render json: @game, status: 201
   end
 
